8807eb3b8105c92918f563d5e8039f3ce64b11ca
dpawlowski <nazguleczek@gmail.com>
2026-02-28 17:59:42 +0100

    Bez vat

--- src/main/kotlin/pl/warmescape/obsluga_leadow/fakturaxl/FakturaXlConnector.kt
+++ src/main/kotlin/pl/warmescape/obsluga_leadow/fakturaxl/FakturaXlConnector.kt
@@ -170,7 +170,7 @@ class FakturaXlConnector {
         }
         return Dokument(
             apiToken = token,
-            typFaktury = if (deal.seller == Seller.KARAS) 7 else invoiceType.fakturaXlType,
+            typFaktury = if (deal.seller == Seller.KARAS) 6 else invoiceType.fakturaXlType,
             typFakturPodtyp = 0,
             obliczajSumeWartosciFakturyWg = 0,
             dataWystawienia = LocalDate.now().toString(),
